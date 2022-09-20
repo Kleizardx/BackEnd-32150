b169867e8f31b0749f1daf20f728f631c79bc2bb
Lucas <kleizardx@gmail.com>
2022-09-20 00:30:48 -0300

    Funciones actualizadas, addPet | addBook

--- clases.js
+++ clases.js
@@ -1,58 +1,55 @@
 class Persona {
-     constructor(nombre, apellido, edad, mascotas, libros){
-          this.nombre = nombre;
-          this.apellido = apellido;
-          this.edad = edad;
-          this.mascotas = mascotas;
-       		this.libros = libros;
-     }
-  
-  //---1----Function getFullName(): String
-	// Retorna el nombre completo del usuario, utulizar template strings.
-	getFullName(){
-    console.log(`${this.nombre} ${this.apellido}`)
-	}
-  	
-  //---2----Function adMascota(String): void
-	// Recibe un nombre de Mascota y lo agrega al Array de Mascotas.
-	addPet(nombre){
-		const newPet = nombre;
-  	console.log(`Mascota agregada ${newPet}`);
- 		this.mascotas.push(newPet);
-		}
-  
-  //---3----Function CountMascotas(): Number.
-	// Retorna la cantidad de mascotas y lo agrega al Array de Mascotas.
-  petCount(){
-    console.log(`${this.mascotas.length}`)
+  constructor(nombre, apellido, edad, mascotas, libros) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.mascotas = mascotas;
+    this.libros = libros;
   }
-  
+
+  //---1----Function getFullName(): String
+  // Retorna el nombre completo del usuario, utulizar template strings.
+  getFullName() {
+    return (`${this.nombre} ${this.apellido}`)
+  }
+
+  //---2----Function adMascota(String): void
+  // Recibe un nombre de Mascota y lo agrega al Array de Mascotas.
+   addPet (newPet) {
+    this.mascotas.push(newPet);
+  }
+
+  //---3----Function CountMascotas(): Number.
+  // Retorna la cantidad de mascotas y lo agrega al Array de Mascotas.
+  petCount() {
+    return (`${this.mascotas.length}`)
+  }
+
   //---4----Function addBook(String, String): void
-	// REcibe un string 'nombre' y un string 'autor' y debe agregar un objeto: 		{nombre: String, autor: String} ak arrat de Libros
-  addBook(titulo, autor){
-    const newBook = {titulo: titulo, autor: autor};
+  // REcibe un string 'nombre' y un string 'autor' y debe agregar un objeto: 		{nombre: String, autor: String} ak arrat de Libros
+  addBook(titulo, autor) {
+    const newBook = { titulo: titulo, autor: autor };
     console.log(`Libro agregado ${newBook.titulo} de ${newBook.autor}`)
     this.libros.push(newBook);
   }
-  
-  //---5----Function getBookNames(): String[]
-	// Retorna un Array con solo los nombres del Array de libros del 		   	 Usuario.
-  getBookNames(){
-    console.log(`${this.libros}`)
-    console.log(this.libros)
-  }
-  
-}
 
-const p = new Persona ("Lucas", "Fernandez", 28, ["Cuco"], [{titulo: "The Witcher", autor:"Andrzej Sapkowski"}]) 
+  //---5----Function getBookNames(): String[]
+  // Retorna un Array con solo los nombres del Array de libros del 		   	 Usuario.
+  getBookNames() {
+    return this.libros.map(book => book.autor)
+    } 
+  }
+
+
+const p = new Persona("Lucas", "Fernandez", 28, ["Cuco"], [{ titulo: "The Witcher", autor: "Andrzej Sapkowski" }])
 
 //Nombre completo del Usuario.
 p.getFullName()
 
 //Agrega una mascota o mas.
 p.addPet('Klei')
-p.addPet('Negruto') 
-p.addPet('Lilith') 
+p.addPet('Negruto')
+p.addPet('Lilith')
 //Revisamos la cantidad de mascotas.
 console.log(p.mascotas)
 p.petCount()
@@ -61,4 +58,7 @@ p.petCount()
 p.addBook("The Lord of the Rings", "J. R. R. Tolkien")
 console.log(p.libros)
 
-p.getBookNames()
+console.log(p.getBookNames())
+
+p.addPet('Cross')
+console.log(p.mascotas)
